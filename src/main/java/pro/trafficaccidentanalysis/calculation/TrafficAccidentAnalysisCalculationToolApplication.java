f1312da09f4887593d09354462c4141aadc09c99
Milan <Milan@192.168.1.7>
2020-10-10 14:23:36 +0200

    initial project setup

---
+++ src/main/java/pro/trafficaccidentanalysis/calculation/TrafficAccidentAnalysisCalculationToolApplication.java
@@ -0,0 +1,13 @@
+package pro.trafficaccidentanalysis.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficAccidentAnalysisCalculationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficAccidentAnalysisCalculationToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/trafficaccidentanalysis/calculation/TrafficAccidentAnalysisCalculationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.trafficaccidentanalysis.calculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficAccidentAnalysisCalculationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
