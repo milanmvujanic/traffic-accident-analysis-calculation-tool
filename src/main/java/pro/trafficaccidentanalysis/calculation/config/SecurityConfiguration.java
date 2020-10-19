50923f08483726fda67f9b0dccbff086f40011b9
Milan <Milan@host.docker.internal>
2020-10-19 11:12:36 +0200

    setting for docker

--- src/main/java/pro/trafficaccidentanalysis/calculation/config/SecurityConfiguration.java
+++ src/main/java/pro/trafficaccidentanalysis/calculation/config/SecurityConfiguration.java
@@ -40,11 +40,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/registration**", "/login**", "/css/**", "/js/**", "/img/**", "/resources/lang/**").permitAll().anyRequest()
-				.authenticated().and().formLogin().loginPage("/login").permitAll().and().logout()
-				.invalidateHttpSession(true).clearAuthentication(true)
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?success")
-				.permitAll();
+		http.authorizeRequests()
+				.antMatchers("/registration**", "/login**", "/css/**", "/js/**", "/img/**", "/resources/lang/**")
+				.permitAll().anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").permitAll().and().logout().invalidateHttpSession(true)
+				.clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutSuccessUrl("/login?success").permitAll();
 	}
 
 }
