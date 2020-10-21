b560404d488904b33352a299d12f1bb6055298ea
Milan <Milan@host.docker.internal>
2020-10-21 19:21:37 +0200

    updated js file for i18

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -3,11 +3,11 @@ function changeLocale(localeData) {
 	var logoutSuccessIndex = url.indexOf('?success');
 	var loginErrorIndex = url.indexOf('?error');
 	var localeDataIndex = url.indexOf('?localeData=');
+	var appendLocaleIndex;
 
 	if (localeDataIndex != -1) {
-		localeDataIndex += '?localeData='.length;
-		var toReplace = url.substr(localeDataIndex, 2);
-		url = url.replace(toReplace, localeData);
+		appendLocaleIndex = localeDataIndex + '?localeData='.length;
+		url = url.substring(0, appendLocaleIndex) + localeData;
 	} else {
 		if (logoutSuccessIndex != -1) {
 			url = url.replace('?success', '');
